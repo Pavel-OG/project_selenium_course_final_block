551774e38155b9b3b9ef03e8afa8a102a0807ec6
Pavel_OG <pavel.ogir22@gmail.com>
2023-07-19 17:59:29 +0400

    Добавлен файл login_page

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from.locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in "http://selenium1py.pythonanywhere.com/ru/accounts/login/", 'element login is not in URL'
+        assert True
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*MainPageLocators.LOGIN_FORM), 'Login form element not found
+'
+        assert True
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*MainPageLocators.REGISTRATION_FORM), 'Registration form element not found
+'
+        assert True

0f1232f686e64bdf118366714c7716f8352dbd63
Pavel_OG <pavel.ogir22@gmail.com>
2023-07-19 17:57:12 +0400

    Добавлены locators Добавлены login_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 
+
 class BasePage():
-    def __init__(self,browser,url,timeout=10):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -12,6 +13,6 @@ class BasePage():
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except (NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
